0c7114fdd100c23422ff6a917982c2bcfdd10975
Carlos Figueiredo <cvieirasp@gmail.com>
2021-06-17 16:37:41 -0300

    posts component

--- src/client.js
+++ src/client.js
@@ -2,5 +2,8 @@ import sanityClient from '@sanity/client';
 
 export default sanityClient({
   projectId: "odku965i",
-  dataset: "production"
+  dataset: "production",
+  // apiVersion: '2021-03-25', // use a UTC date string
+  // token: 'sanity-auth-token', // or leave blank for unauthenticated usage
+  // useCdn: true, // `false` if you want to ensure fresh data
 });
