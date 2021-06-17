74ca5818525bacd0b6b5be314873c7006b99c9c1
Carlos Figueiredo <cvieirasp@gmail.com>
2021-06-17 14:28:49 -0300

    home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <main className="bg-gray-700">
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Olá, eu sou Carlos!</h1>
+      </section>
+    </main>
   );
 }
